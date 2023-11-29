8417f8c6ef213440458dc41a8c2d78aef6281ad7
Joey Leung <joey.leung23@imperial.ac.uk>
2023-11-29 13:01:08 +0000

    formatting changes

--- api/app.py
+++ api/app.py
@@ -1,21 +1,12 @@
 from flask import Flask, render_template, request
-import re
 import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
-
-
-@app.route("/submit", methods=["POST"])
-def submit():
-    input_name = request.form.get("name")
-    input_age = request.form.get("age")
-    link = "https://picsum.photos/" + input_name + "/" + input_age
-    return render_template("hello.html", link=link)
 
 
 @app.route("/apis", methods=["GET", "POST"])
@@ -55,42 +46,3 @@ def handle_query():
 def process_query(word):
     if "dinosaurs" in word:
         return "Dinosaurs ruled the Earth 200 million years ago"
-
-    if "asteroids" in word:
-        return "Unknown"
-
-    if word == "What is your name?":
-        return "Howell_Karla_Joey"
-
-    if "plus" in word:
-        matches = re.findall(r"\d+", word)
-        integers = [int(match) for match in matches]
-        return str(sum(integers))
-
-    if "largest" in word:
-        matches = re.findall(r"\d+", word)
-        integers = [int(match) for match in matches]
-        return str(max(integers))
-
-    if "multiplied" in word:
-        matches = re.findall(r"\d+", word)
-        integers = [int(match) for match in matches]
-        return str(integers[0] * integers[1])
-
-    def test_six(x):
-        y = x ** (1 / 6)
-        y = int(y)
-        if y**6 == x:
-            return True
-        else:
-            return False
-
-    if "square" in word and "cube" in word:
-        matches = re.findall(r"\d+", word)
-        integers = [int(match) for match in matches]
-        for x in integers:
-            if test_six(x):
-                return str(x)
-        return ""
-    else:
-        return str(100)
