69ec2680e18404327d304c3b82596402b3a34d36
Howell Liu <howell@dyn3201-56.wlan.ic.ac.uk>
2023-12-05 12:44:11 +0000

    Wrote the mock, the api function call, and uploaded a mock json file for a journey. Haven't tested because unsure how to hide api_key

--- api/app_test.py
+++ api/app_test.py
@@ -1,36 +1,26 @@
 from app import process_query
+import pytest
+import json
+from helpers import helpers
+#Will need to have downloaded pytest-mock
 
-"""
+@pytest.fixture()
+def fake_tfl_journey():
+    """
+    Fake TFL Journey from London Bridge (EC4R9HA) to Imperial College (SW72BX)
+    """
+    with open("sample_data/mockTFLJourney.json") as f:
+        return json.load(f)
 
-def test_knows_about_dinosaurs():
-    assert (
-        process_query("dinosaurs") == "Dinosaurs \
-            ruled the Earth 200 million years ago"
-    )
+def test_retrieve_transit_time_using_mocks(mocker, fake_tfl_journey):
+    """
+    
+    """
+    fake_resp = mocker.Mock()
+    fake_resp.json = mocker.Mock(return_value=fake_tfl_journey)
+    fake_resp.status_code = HTTPStatus.OK
 
+    mocker.patch("tfl_app.requests.get",return_value=fake_resp)
 
-def test_does_not_know_about_asteroids():
-    assert process_query("asteroids") == "Unknown"
-
-
-def test_knows_about_name():
-    assert process_query("What is your name?") == "Howell_Karla_Joey"
-
-
-def test_add():
-    assert process_query("What is 2 plus 7?") == "9"
-
-
-def test_multiply():
-    assert process_query("What is 2 multiplied by 7?") == "14"
-
-
-def test_largest_number():
-    assert (
-        process_query(
-            "Which of the following \
-                           numbers is the largest: 1, 2, 3?"
-        )
-        == "3"
-    )
-"""
+    journey_info = retrieve_tfl_journey("EC4R9HA","SW72BX","")
+    assert journey_info == JourneyInfo.from_dict(fake_tfl_journey)
--- api/helpers/helpers.py
+++ api/helpers/helpers.py
@@ -2,15 +2,47 @@
 Helper functions for the website
 """
 import psycopg as db
+import requests
+from dataclasses import dataclass
 
-
+BASE_URL = "https://api.tfl.gov.uk/Journey/JourneyResults/"
+API = BASE_URL + "{postcode_start}/to/{postcode_end}?api_key={api_key}"
 
 def parse_postcode(postcode):
     assert(isinstance(postcode, str))
     parsed_postcode = postcode.replace(" ", "")
     return parsed_postcode
 
-def querydb_postcodes():
+def tfl_journey(start, end, key):
+    parsed_start = parse_postcode(start)
+    parsed_end = parse_postcode(end)
+    url = API.format(postcode_start = parsed_start, postcode_end = parsed_end, api_key = key)
+    resp = requests.get(url)
+    return resp.json()
 
+@dataclass
+class JourneyLegInfo:
+    duration: float
+    instruction: str
+
+@dataclass
+class JourneyInfo:
+    duration: float
+    legs: list(JourneyLegInfo)
+
+    @classmethod
+    def from_dict(cls, data:dict) -> "JourneyInfo":
+        return cls(
+            duration = data["journeys"]["duration"],
+            legs = [JourneyLegInfo(leg["duration"],leg["instruction"]["summary"]) for leg in data["journeys"]["legs"]]
+        )
     
+def retrieve_tfl_journey(start: str, end: str, key: str) -> JourneyInfo:
+    """
+    Does the API call for the TFL Journey and returns a JourneyInfo Class Object
+    """
+    data = tfl_journey(start, end, key)
+    return JourneyInfo.from_dict(data)
 
+def querydb_postcodes():
+    return None 
